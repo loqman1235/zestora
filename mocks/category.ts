6dea1049faf41338bdc403456dee27870cafbb7a
loqman1235 <tonyvito12@gmail.com>
2025-03-06 06:25:29 +0100

    Add mock category data with subcategories for Men, Women, and Kids

---
+++ mocks/category.ts
@@ -0,0 +1,115 @@
+import { Categories } from "@/types/category";
+
+export const categories: Categories = [
+  {
+    id: "1",
+    name: "Men",
+    slug: "men",
+    subCategories: [
+      {
+        id: "1",
+        name: "Shirts",
+        slug: "shirts",
+      },
+      {
+        id: "2",
+        name: "Jeans",
+        slug: "jeans",
+      },
+      {
+        id: "3",
+        name: "Jackets",
+        slug: "jackets",
+      },
+      {
+        id: "4",
+        name: "Pants",
+        slug: "pants",
+      },
+      {
+        id: "5",
+        name: "Shoes",
+        slug: "shoes",
+      },
+      {
+        id: "6",
+        name: "Accessories",
+        slug: "accessories",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Women",
+    slug: "women",
+    subCategories: [
+      {
+        id: "1",
+        name: "Shirts",
+        slug: "shirts",
+      },
+      {
+        id: "2",
+        name: "Jeans",
+        slug: "jeans",
+      },
+      {
+        id: "3",
+        name: "Jackets",
+        slug: "jackets",
+      },
+      {
+        id: "4",
+        name: "Pants",
+        slug: "pants",
+      },
+      {
+        id: "5",
+        name: "Shoes",
+        slug: "shoes",
+      },
+      {
+        id: "6",
+        name: "Accessories",
+        slug: "accessories",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Kids",
+    slug: "kids",
+    subCategories: [
+      {
+        id: "1",
+        name: "Shirts",
+        slug: "shirts",
+      },
+      {
+        id: "2",
+        name: "Jeans",
+        slug: "jeans",
+      },
+      {
+        id: "3",
+        name: "Jackets",
+        slug: "jackets",
+      },
+      {
+        id: "4",
+        name: "Pants",
+        slug: "pants",
+      },
+      {
+        id: "5",
+        name: "Shoes",
+        slug: "shoes",
+      },
+      {
+        id: "6",
+        name: "Accessories",
+        slug: "accessories",
+      },
+    ],
+  },
+];
