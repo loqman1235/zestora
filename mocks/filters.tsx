f78f5829e7a2ea7f81056cfc254d90f0ea5b099e
loqman1235 <tonyvito12@gmail.com>
2025-03-12 06:49:43 +0100

    Add mock filter data for categories, genders, colors, and sizes

---
+++ mocks/filters.tsx
@@ -0,0 +1,23 @@
+export const categories = [
+  "Shirts",
+  "Jeans",
+  "Jackets",
+  "Shoes",
+  "Accessories",
+];
+export const genders = ["Men", "Women"];
+export const colors = [
+  "red",
+  "blue",
+  "green",
+  "black",
+  "white",
+  "gray",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+  "brown",
+  "beige",
+];
+export const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
