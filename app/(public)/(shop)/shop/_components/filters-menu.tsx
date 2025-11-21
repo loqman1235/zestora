c8c90db3ccffa5798e9b843ccc65bc2946ed3edc
loqman1235 <tonyvito12@gmail.com>
2025-11-21 16:52:28 +0100

    chore: comment unused filters-menu.tsx to fix production build errors

--- app/(public)/(shop)/shop/_components/filters-menu.tsx
+++ app/(public)/(shop)/shop/_components/filters-menu.tsx
@@ -1,19 +1,19 @@
-import { Separator } from "@/components/ui/separator";
-import { SlidersVerticalIcon } from "lucide-react";
-import { FiltersContent } from "./filters-content";
+// import { Separator } from "@/components/ui/separator";
+// import { SlidersVerticalIcon } from "lucide-react";
+// import { FiltersContent } from "./filters-content";
 
-export const FiltersMenu = () => {
-  return (
-    // Desktop Filters
-    <>
-      <aside className="border-border hidden h-fit w-1/4 rounded-lg border p-5 md:block">
-        <div className="flex items-center justify-between">
-          <h4 className="text-lg font-bold">Filters</h4>
-          <SlidersVerticalIcon className="text-muted-foreground size-5" />
-        </div>
-        <Separator className="my-5" />
-        <FiltersContent />
-      </aside>
-    </>
-  );
-};
+// export const FiltersMenu = () => {
+//   return (
+//     // Desktop Filters
+//     <>
+//       <aside className="border-border hidden h-fit w-1/4 rounded-lg border p-5 md:block">
+//         <div className="flex items-center justify-between">
+//           <h4 className="text-lg font-bold">Filters</h4>
+//           <SlidersVerticalIcon className="text-muted-foreground size-5" />
+//         </div>
+//         <Separator className="my-5" />
+//         <FiltersContent />
+//       </aside>
+//     </>
+//   );
+// };
