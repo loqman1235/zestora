1ba5a8af92aba8ccbfb82c6998822363861ad9f0
loqman1235 <tonyvito12@gmail.com>
2025-11-21 08:54:37 +0100

    feat: prevent logged in users from accessing auth pages

--- app/(public)/(auth)/layout.tsx
+++ app/(public)/(auth)/layout.tsx
@@ -1,10 +1,16 @@
+import { auth } from "@/auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Account",
 };
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
+  if (session && session.user) return redirect("/");
+
   return (
     <div className="mt-10 flex flex-col items-center justify-center">
       {children}
